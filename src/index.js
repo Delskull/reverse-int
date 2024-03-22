622a5196b2da607412182fb3a15c6fcb1344c431
delskull <Deluta1989@gmail.com>
2024-03-22 15:16:49 +0300

    feat: finita la comedia

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+    const arrayOfStrings = Array.from(String(n))
+    // .reverse().join('')
+  let filter = arrayOfStrings.filter((item)=> item >= 0)
+  if ((filter[filter.length - 1]) === '0'){
+   filter.pop()
+   return filter.reverse().join('')
+  }
+    // for( i = 0 ; i < arrayOfStrings.length; i++){
+
+    // }
+  return filter.reverse().join('')
+  }
